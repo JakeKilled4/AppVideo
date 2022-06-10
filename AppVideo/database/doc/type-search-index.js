ac50840b878673def8d9bf29e28931f4880f4b2d
Marcos Gil <marcos.gile@protonmail.com>
2022-06-10 18:18:33 +0200

    Updated the database

---
+++ AppVideo/database/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"tds.driver","l":"FactoriaServicioPersistencia"},{"p":"tds.driver","l":"ServicioPersistencia"}];updateSearchResults();
