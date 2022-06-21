c416392a59b3e25ff2a88bd0880f7b1ae8858b60
Marcos Gil Espinosa <marcos.gile@proton.me>
2022-06-21 17:04:53 +0200

    Refactoring en SearchPanel: dividido el método createInnerPannel en varios submétodos para facilitar la lectura del código

--- AppVideo/src/main/java/um/tds/projects/appvideo/view/SearchPanel.java
+++ AppVideo/src/main/java/um/tds/projects/appvideo/view/SearchPanel.java
@@ -26,8 +26,26 @@ public class SearchPanel extends CommonPanel {
 	
 	@Override
 	protected JPanel createInnerPanel() {
+		// Define an entry for each video.
+		List<ListEntry> entries = controller.getSearchedVideos()
+			.stream ()
+			.map    (v -> new VideoListEntry(mainWindow, v))
+			.collect(Collectors.toList());
+		// Create an info entry and place it on top of the list.
+		// This entry indicates whether we are showing all of the videos
+		// or just the results of a search.
+		entries.add(0, makeInfoEntry());
+		
+		return new CommonListPanel(mainWindow, entries);
+	}
+	
+	@Override
+	protected JPanel createRightPanel() {
+		return new LabelPanel(controller.getAllLabels());
+	}
 
-		ListEntry info = new ListEntry(mainWindow) {
+	private ListEntry makeInfoEntry() {
+		return new ListEntry(mainWindow) {
 			{
 				addComponents();
 			}
@@ -35,53 +53,49 @@ public class SearchPanel extends CommonPanel {
 			protected JPanel createInnerPanel() {
 				ComponentFactory componentFactory = ComponentFactory.getUniqueInstance();
 				JPanel p = new JPanel();
-				p.setBackground(getBackground());
-				p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+				p.setLayout    (new BoxLayout(p, BoxLayout.Y_AXIS));
 				p.setAlignmentX(CENTER_ALIGNMENT);
 				
-				boolean noLabels = controller.getSelectedLabels().isEmpty();
-				String searchTitle = controller.getSearchTitle();
-				String titleText = "Title: \""+searchTitle+"\"";
-				if(searchTitle.isBlank() && noLabels) {
-					JLabel allVideosLabel = componentFactory.specialLabel("All videos");
-					allVideosLabel.setFont(new Font("Default",Font.ITALIC,17));
-					allVideosLabel.setAlignmentX(CENTER_ALIGNMENT);
-					p.add(allVideosLabel);
-				}
-				else {
-					JLabel searchResultLabel = componentFactory.specialLabel("Search result");
-					searchResultLabel.setFont(new Font("Default",Font.ITALIC,17));
-					searchResultLabel.setAlignmentX(CENTER_ALIGNMENT);
-					p.add(searchResultLabel);
+				boolean noLabels    = controller.getSelectedLabels().isEmpty();
+				String  searchTitle = controller.getSearchTitle   ();
+				String  titleText   = "Title: \""+searchTitle+"\"";
+				if (searchTitle.isBlank() && noLabels) {
+					p.add(createAllVideosLabel(componentFactory));
+				} else {
+					p.add(createSearchResultLabel(componentFactory));
 					p.add(Box.createRigidArea(new Dimension(0,10)));
-					if(!searchTitle.isBlank()) {
-						JLabel titleLabel = componentFactory.specialLabel(titleText);
-						titleLabel.setAlignmentX(CENTER_ALIGNMENT);
-						p.add(titleLabel);
-					}
-					if(!noLabels) {
-						JLabel labelsLabel = componentFactory.specialLabel("Labels: selected labels");
-						labelsLabel.setAlignmentX(CENTER_ALIGNMENT);
-						p.add(labelsLabel);
-					}
+					if (!searchTitle.isBlank()) p.add(createTitleLabel (componentFactory, titleText));
+					if (!noLabels)              p.add(createLabelsLabel(componentFactory));
 				}
-				
 				return p;
 			}
-		};
-		
-		List<ListEntry> entries = controller.getSearchedVideos()
-				.stream()
-				.map(v -> new VideoListEntry(mainWindow, v))
-				.collect(Collectors.toList());
-		
-		entries.add(0,info);
-		return new CommonListPanel(mainWindow, entries);
-	}
 
-	@Override
-	protected JPanel createRightPanel() {
-		return new LabelPanel(controller.getAllLabels());
+			private JLabel createLabelsLabel(ComponentFactory componentFactory) {
+				JLabel labelsLabel = componentFactory.specialLabel("Labels: selected labels");
+				labelsLabel.setAlignmentX(CENTER_ALIGNMENT);
+				return labelsLabel;
+			}
+
+			private JLabel createTitleLabel(ComponentFactory componentFactory, String titleText) {
+				JLabel titleLabel = componentFactory.specialLabel(titleText);
+				titleLabel.setAlignmentX(CENTER_ALIGNMENT);
+				return titleLabel;
+			}
+
+			private JLabel createSearchResultLabel(ComponentFactory componentFactory) {
+				JLabel searchResultLabel = componentFactory.specialLabel("Search result");
+				searchResultLabel.setFont(new Font("Default",Font.ITALIC,17));
+				searchResultLabel.setAlignmentX(CENTER_ALIGNMENT);
+				return searchResultLabel;
+			}
+			
+			private JLabel createAllVideosLabel(ComponentFactory componentFactory) {
+				JLabel allVideosLabel = componentFactory.specialLabel("All videos");
+				allVideosLabel.setFont      (new Font("Default", Font.ITALIC, 17));
+				allVideosLabel.setAlignmentX(CENTER_ALIGNMENT);
+				return allVideosLabel;
+			}
+		};
 	}
 
 }
